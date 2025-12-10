1c417eb094301b5c3cda1407ef4dcfa389f062df
Meluwor <mukeratov@web.de>
2025-12-10 10:28:50 +0100

    added some more functions to create a new html with the given template and the animal data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -46,11 +46,43 @@ def print_animal_info(animals_data):
         print()
         print()
 
+def get_animal_string(animals_data):
+    """
+    This function creates a string of all animals
+    """
+    animal_string =""
+    for animal in animals_data:
+        animal_name, animal_diet, animal_type, animal_location = get_animal_info(animal)
+        if animal_name:
+            animal_string += f'Name: {animal_name}\n'
+        if animal_diet:
+            animal_string += f'Diet: {animal_diet}\n'
+        if animal_location:
+            animal_string += f'Location: {animal_location}\n'
+        if animal_type:
+            animal_string += f'Type: {animal_type}\n'
+        animal_string +="\n"
+    return animal_string
+
+
+def get_template_as_string(path):
+    with open(path, "r") as a:
+        data = a.read()
+    return data
+
+
+def save_data(path,data):
+    with open(path,"w")as a:
+        a.write(data)
+
 
 def main():
     animals_data = load_data('animals_data.json')
-    print_animal_info(animals_data)
-
+    template_string = get_template_as_string("animals_template.html")
+    animal_string = get_animal_string(animals_data)
+    new_template_string = template_string.replace("__REPLACE_ANIMALS_INFO__", animal_string)
+    save_data("animals.html",new_template_string)
+    print(new_template_string)
 
 if __name__ == "__main__":
     main()
