df0437b436a2a956791d0201c0fe3957c8410693
Meluwor <mukeratov@web.de>
2025-12-15 13:21:52 +0100

    added a new funtion to serialize an animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,25 +9,49 @@ def load_data(file_path):
 
 def get_animal_info(animal):
     """
-    This function gets all info of an animal if possible
+    This function gets all info of an animal if possible and returns a "new animal" which just
+    have the needed info to display.
     """
-    animal_name = None
-    animal_diet = None
-    animal_type = None
-    animal_location = None
+    new_animal = {"name": None, "diet": None, "type": None, "location": None}
     if "name" in animal:
-        animal_name = animal["name"]
+        new_animal["name"] = animal["name"]
     if "characteristics" in animal:
         character = animal["characteristics"]
         if "diet" in character:
-            animal_diet = character["diet"]
+            new_animal["diet"] = character["diet"]
         if "type" in character:
-            animal_type = character["type"]
+            new_animal["type"] = character["type"]
     if "locations" in animal:
         if len(animal["locations"]) > 0:
-            animal_location = animal["locations"][0]
-    return animal_name, animal_diet, animal_type, animal_location
+            new_animal["location"] = animal["locations"][0]
+    return new_animal
 
+
+def serialize_animal(animal):
+    """
+    This function serialize an animal.
+    """
+    new_animal = get_animal_info(animal)
+    animal_name = new_animal["name"]
+    animal_diet = new_animal["diet"]
+    animal_type = new_animal["type"]
+    animal_location = new_animal["location"]
+
+    animal_string = ""
+    animal_string += '<li class="cards__item">\n'
+    if animal_name:
+        animal_string += f'<div class ="card__title" > {animal_name} </div>\n'
+    animal_string += f'<p class="card__text">'
+    if animal_diet:
+        animal_string += f'<strong>Diet:</strong> {animal_diet}<br/>\n'
+    if animal_location:
+        animal_string += f'<strong>Location:</strong> {animal_location}<br/>\n'
+    if animal_type:
+        animal_string += f'<strong>Type:</strong> {animal_type}<br/>\n'
+    animal_string += '</p>\n'
+    animal_string += "</li>\n"
+
+    return animal_string
 
 def get_animal_string(animals_data):
     """
@@ -35,20 +59,7 @@ def get_animal_string(animals_data):
     """
     animal_string =""
     for animal in animals_data:
-        animal_name, animal_diet, animal_type, animal_location = get_animal_info(animal)
-
-        animal_string += '<li class="cards__item">\n'
-        if animal_name:
-            animal_string += f'<div class ="card__title" > {animal_name} </div>\n'
-        animal_string += f'<p class="card__text">'
-        if animal_diet:
-            animal_string += f'<strong>Diet:</strong> {animal_diet}<br/>\n'
-        if animal_location:
-            animal_string += f'<strong>Location:</strong> {animal_location}<br/>\n'
-        if animal_type:
-            animal_string += f'<strong>Type:</strong> {animal_type}<br/>\n'
-        animal_string += '</p>\n'
-        animal_string +="</li>\n"
+        animal_string += serialize_animal(animal)
     return animal_string
 
 
