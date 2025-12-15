dc7c0defe011b009f0e92842d03464b1ee1b30ca
Meluwor <mukeratov@web.de>
2025-12-15 13:56:33 +0100

    added three new atributes to the animal (color, skin_type, lifespan)

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,15 +12,24 @@ def get_animal_info(animal):
     This function gets all info of an animal if possible and returns a "new animal" which just
     have the needed info to display.
     """
-    new_animal = {"name": None, "diet": None, "type": None, "location": None}
+    new_animal = {"name": None, "diet": None, "type": None, "location": None, "color": None,
+                  "skin_type": None, "lifespan": None}
     if "name" in animal:
         new_animal["name"] = animal["name"]
+
     if "characteristics" in animal:
         character = animal["characteristics"]
         if "diet" in character:
             new_animal["diet"] = character["diet"]
         if "type" in character:
             new_animal["type"] = character["type"]
+        if "color" in character:
+            new_animal["color"] = character["color"]
+        if "skin_type" in character:
+            new_animal["skin_type"] = character["skin_type"]
+        if "lifespan" in character:
+            new_animal["lifespan"] = character["lifespan"]
+
     if "locations" in animal:
         if len(animal["locations"]) > 0:
             new_animal["location"] = animal["locations"][0]
@@ -36,9 +45,13 @@ def serialize_animal(animal):
     animal_diet = new_animal["diet"]
     animal_type = new_animal["type"]
     animal_location = new_animal["location"]
+    animal_color = new_animal["color"]
+    animal_skin_type = new_animal["skin_type"]
+    animal_lifespan = new_animal["lifespan"]
 
     animal_string = ""
     animal_string += '<li class="cards__item">\n'
+    #I expect a name for each animal else this needs to be changed a bit
     if animal_name:
         animal_string += f'<div class ="card__title" > {animal_name} </div>\n'
     animal_string += f'<p class="card__text">'
@@ -48,6 +61,13 @@ def serialize_animal(animal):
         animal_string += f'<strong>Location:</strong> {animal_location}<br/>\n'
     if animal_type:
         animal_string += f'<strong>Type:</strong> {animal_type}<br/>\n'
+    if animal_color:
+        animal_string += f'<strong>Color:</strong> {animal_color}<br/>\n'
+    if animal_skin_type:
+        animal_string += f'<strong>Skin type:</strong> {animal_skin_type}<br/>\n'
+    if animal_lifespan:
+        animal_string += f'<strong>Skin type:</strong> {animal_lifespan}<br/>\n'
+
     animal_string += '</p>\n'
     animal_string += "</li>\n"
 
